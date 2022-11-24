9d75dfb8b5ca6f8c7890ab86cdb4f751f3e951fb
Gabriel S <gbss91@gmail.com>
2022-11-24 15:23:57 +0000

    Updated Query

--- lib/mysearch.rb
+++ lib/mysearch.rb
@@ -1,23 +1,24 @@
 class MySearch
 
   def self.booking(attributes, keyword)
-    query_attrs = attributes.map { |a| "#{a.to_s} LIKE :search_values"}.join(' OR ')
-    Booking.where(query_attrs, search_values: "%#{keyword.downcase}%")
+    query_attrs = attributes.map { |a| "#{a.to_s}::TEXT ILIKE '%#{keyword}%'"}.join(' OR ')
+    Booking.where(query_attrs)
   end
 
   def self.user(attributes, keyword)
-    query_attrs = attributes.map { |a| "#{a.to_s} LIKE :search_values"}.join(' OR ')
-    User.where(query_attrs, search_values: "%#{keyword.downcase}%")
+    query_attrs = attributes.map { |a| "#{a.to_s}::TEXT ILIKE '%#{keyword}%'"}.join(' OR ')
+    puts query_attrs
+    User.where(query_attrs)
   end
 
   def self.flight(attributes, keyword)
-    query_attrs = attributes.map { |a| "#{a.to_s} LIKE :search_values"}.join(' OR ')
-    Flight.where(query_attrs, search_values: keyword.downcase)
+    query_attrs = attributes.map { |a| "#{a.to_s}::TEXT ILIKE '%#{keyword}%'"}.join(' OR ')
+    Flight.where(query_attrs)
   end
 
   def self.hotel(attributes, keyword)
-    query_attrs = attributes.map { |a| "#{a.to_s} LIKE :search_values"}.join(' OR ')
-    Hotel.where(query_attrs, search_values: keyword.downcase)
+    query_attrs = attributes.map { |a| "#{a.to_s}::TEXT ILIKE '%#{keyword}%'"}.join(' OR ')
+    Hotel.where(query_attrs)
   end
 
 end
